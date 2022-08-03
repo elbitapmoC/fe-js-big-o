5c4f9031ec03fc41cadb67da56ecb272c9f63303
Ahkal <bp@sojourn.llc>
2022-08-02 21:11:42 -0400

    big O notation examples.

---
+++ script.js
@@ -0,0 +1,52 @@
+const data = ["A", "B", "C"];
+const data2 = [1, 2, 3, 44, 5, 6, 7, 8, , 9];
+// SECTION ---------------------- START
+
+// n - length of our data array.
+for (let i = 0; i < data2.length; i++) {
+  console.log(data2[i]);
+}
+for (let j = 0; j < data2.length; j++) {
+  console.log(data[j]);
+}
+// SECTION ---------------------- END
+
+// SECTION ---------------------- START
+
+// After looping through both FOR's
+// O(n + a)
+
+// O(n * a)
+// a
+for (let i = 0; i < data2.length; i++) {
+  // n
+  for (let j = 0; j < data2.length; j++) {
+    console.log(data[i] + data2[j]);
+  }
+}
+// SECTION ---------------------- END
+
+// SECTION ---------------------- START
+// O(n^2)
+// n
+for (let i = 0; i < data.length; i++) {
+  // n
+  for (let j = 0; j < data.length; j++) {
+    console.log(data[i] + data[j]);
+  }
+}
+// SECTION ---------------------- END
+
+// SECTION ---------------------- START
+// O(n^2 + n) - Here we would actually remove the n, we don't care about the things that scale Less quickly.
+// O(n^2)     - Updated Big O to this.
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    console.log(data[i]);
+  }
+}
+
+for (let i = 0; i < data.length; i++) {
+  const element = data[i];
+}
+// SECTION ---------------------- END
